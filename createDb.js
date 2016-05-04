9a5aee2b5bd911be8e0b25aa9ccad07839b9058e
an.sokolov <sokolik_85@list.ru>
2016-05-04 17:35:00 +0600

    add test function to create Users

--- createDb.js
+++ createDb.js
@@ -1,16 +1,38 @@
+var mongoose = require('./libs/mongoose');
+var async = require('async');
 var User = require('./models/user').User;
- var http = require('http');
-var user = new User({
-    username: "Tester2",
-    password: "secret"
-});
 
-user.save(function (err,user, affected) {
-    if (err) throw err;
-    console.log(arguments);
 
-    User.findOne({username:"Tester"},function (err, tester) {
-        console.log(tester);
-    })
+mongoose.connection.on('open', function (err, result) {
 
+    var db = mongoose.connection.db;
+    db.dropDatabase(function (err) {
+
+        if(err) throw err;
+
+        async.parallel([
+            function (callback) {
+                var vasya = new User({username:"Вася", password: 'supervasya'});
+                vasya.save(function (err) {
+                    callback(err,vasya);
+                });
+            },
+            function (callback) {
+                var petya = new User({username:"Петя", password: '123'});
+                petya.save(function (err) {
+                    callback(err,petya);
+                })
+            },
+            function (callback) {
+                var admin = new User({username:"admin", password: 'thetruehero'});
+                admin.save(function (err) {
+                    callback(err,admin);
+                })
+            }
+        ], function (err, results) {
+            console.log(arguments);
+            mongoose.disconnect();
+        });
+
+    });
 });
