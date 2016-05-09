6be94da5cd6dc43b67fddd865f66dfa26739a9a6
an.sokolov <sokolik_85@list.ru>
2016-05-09 09:20:55 +0600

    created autorization logic for app

--- app.js
+++ app.js
@@ -28,11 +28,12 @@ app.use(express.session({
     store: new MongoStore({mongooseConnection: mongoose.connection})
 }));
 
-app.use(function (req, res, next) {
-    req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
-    res.send("Visits: " + req.session.numberOfVisits);
-});
+// app.use(function (req, res, next) {
+//     req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+//     res.send("Visits: " + req.session.numberOfVisits);
+// });
 app.use(require('./middleware/sendHttpError'));
+app.use(require('./middleware/loaduser'));
 
 app.use(app.router);
 require('routes')(app);
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,49 @@
-var User = require('models/user').User;
+var User = require('../models/user').User;
 var HttpError = require('error').HttpError;
 var ObjectID = require('mongodb').ObjectID;
+var checkAuth = require('middleware/checkAuth');
 
 module.exports = function (app) {
-  app.get('/', function (req, res, next){
-    res.render('index', {
-    });
-  });
+  app.get('/', require('./frontpage').get);
 
-  var User = require('../models/user').User;
-  app.get('/users', function (req, res, next) {
-    User.find({}, function (err, users) {
-      if(err){
-        return next(err);
-      }
-      res.json(users);
-    })
-  });
+  app.get('/login', require('./login').get);
+  app.post('/login', require('./login').post);
+  app.post('/logout', require('./logout').post);
 
-  app.get('/user/:id', function (req, res, next){
-    try {
-      var id = new ObjectID(req.params.id);
-    } catch (e){
-      next(404);
-      return;
-    }
+  app.get('/chat', checkAuth, require('./chat').get);
 
-    User.findById(id, function (err, user) {
-      if(err){
-        return next(err);
-      }
-      if(!user) {
-        console.log('No user found');
-        next(new HttpError(404, "User not found"));
-      }
-      res.json(user);
-      console.log('req is ' + req.params.id);
-    })
-  });
+
+  // app.get('/users', function (req, res, next) {
+  //   User.find({}, function (err, users) {
+  //     if(err){
+  //       return next(err);
+  //     }
+  //     res.json(users);
+  //   })
+  // });
+  //
+  // app.get('/user/:id', function (req, res, next){
+  //   try {
+  //     var id = new ObjectID(req.params.id);
+  //   } catch (e){
+  //     next(404);
+  //     return;
+  //   }
+  //
+  //   User.findById(id, function (err, user) {
+  //     if(err){
+  //       return next(err);
+  //     }
+  //     if(!user) {
+  //       console.log('No user found');
+  //       next(new HttpError(404, "User not found"));
+  //     }
+  //     res.json(user);
+  //     console.log('req is ' + req.params.id);
+  //   })
+  // });
+
+
 };
 
 // exports.index = function(req, res){
